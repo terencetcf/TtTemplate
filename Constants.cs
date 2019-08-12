9ebd0ec711fc1c575f43a308d24bff711c269aac
Terence Tai <terencetcf@hotmail.com>
2019-08-12 18:27:43 +0100

    first commit

---
+++ Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Tt.NetCoreReactTypeScript
+{
+    public static class Constants
+    {
+        public static string AuthorizationCookieKey => "Auth";
+        public static string HttpContextServiceUserItemKey => "ServiceUser";
+    }
+}
